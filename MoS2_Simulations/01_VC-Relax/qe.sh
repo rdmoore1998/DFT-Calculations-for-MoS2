478b50f231ebed69c27bb5afd10820ff6c338bbe
rdmoore1998 <54477521+rdmoore1998@users.noreply.github.com>
2024-12-18 14:54:50 -0500

    Update qe.sh

--- MoS2_Simulations/01_VC-Relax/qe.sh
+++ MoS2_Simulations/01_VC-Relax/qe.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=00:20:00
 
+module load quantum-espresso/6.6
+
+set nodes = `scontrol show hostname "$SLURM_JOB_NODELIST"`
+
+echo "job = ${SLURM_JOB_NAME}"
+echo "nodes = $nodes"
+echo "Start time = `date +%Y%m%d-%T`"
+
+srun $(which pw.x) -in MoS2.in > MoS2.Energy.out
+
+exit
